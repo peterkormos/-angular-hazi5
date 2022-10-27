3e01f66662d85f0a56a589b0a4e4d8719f3ac835
Kormos Péter <peter.kormos@ujvilag.gov.hu>
2022-10-27 13:29:00 +0200

    kód formázás

--- src/app/pipes/display-pandemic-data.pipe.ts
+++ src/app/pipes/display-pandemic-data.pipe.ts
@@ -2,10 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { PandemicData } from '../app.component';
 
 @Pipe({
-  name: 'displayPandemicData'
+  name: 'displayPandemicData',
 })
 export class DisplayPandemicDataPipe implements PipeTransform {
-
   transform(value: PandemicData, iWantTheTruth?: boolean): string {
     let newTests, inHospital: number;
     let newCases: string;
@@ -14,14 +13,17 @@ export class DisplayPandemicDataPipe implements PipeTransform {
       newTests = value.newTests!;
       newCases = '';
       inHospital = value.inHospital!;
-    }
-    else {
+    } else {
       newTests = 2 * value.newTests!;
-      newCases = `, újonnan azonosított fertőzöttek száma ${Math.round(Math.min(Math.random() * 500, value.newTests! * 0.05))}`;
+      newCases = `, újonnan azonosított fertőzöttek száma ${Math.round(
+        Math.min(Math.random() * 500, value.newTests! * 0.05)
+      )}`;
       inHospital = Math.round(value.inHospital! / 3);
     }
 
-    return `napon a mintavételek száma ${newTests}${newCases}; aminek következtében ${inHospital}-an vannak kórházban${iWantTheTruth ? '.' : ' (mindannyian oltatlan idős krónikus betegek).'}`;
+    return `napon a mintavételek száma ${newTests}${newCases};
+    aminek következtében ${inHospital}-an vannak kórházban${
+      iWantTheTruth ? '.' : ' (mindannyian oltatlan idős krónikus betegek).'
+    }`;
   }
-
 }
